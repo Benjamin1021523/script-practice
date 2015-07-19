582b33f2b3e3e6103a99d1c923f306fd29d07999
s1021523 <benjamin.30805@yahoo.com.tw>
2015-07-19 15:42:26 +0000

    2015/07/19

---
+++ a001.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+input=
+echo -e "please enter your name:\c"
+read $input
+echo $input
+echo "hello, $input"
+./a001.sh
---
+++ a002.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+a=0 #No space
+b=0
+echo -e "Please enter two number for adding:\c"
+read a b #Don't use ',' to partition two variable.
+declare -i ans=$a+$b #Use parameter '-i' can do operation instead two strings merger.
+echo $ans
