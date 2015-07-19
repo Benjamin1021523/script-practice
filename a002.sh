064e448b8931b656d53c4d381cbfa35c0118b245
s1021523 <benjamin.30805@yahoo.com.tw>
2015-07-19 16:08:38 +0000

    Forget to call itself at the end of file.

--- a002.sh
+++ a002.sh
@@ -5,3 +5,4 @@ echo -e "Please enter two number for adding:\c"
 read a b #Don't use ',' to partition two variable.
 declare -i ans=$a+$b #Use parameter '-i' can do operation instead two strings merger.
 echo $ans
+./a002.sh
